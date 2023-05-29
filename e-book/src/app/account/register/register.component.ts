3dd57fe4a7f4dd78b7c3ee97626b064bc55be96e
Sunil Salaria <hardeep.singh@lpinfotech.com>
2023-05-29 13:22:38 +0530

    update validation form

--- e-book/src/app/account/register/register.component.ts
+++ e-book/src/app/account/register/register.component.ts
@@ -13,11 +13,21 @@ export class RegisterComponent {
     city: new FormControl('',Validators.required),
     email: new FormControl('',[Validators.required,Validators.email]),
     password: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(12)]),
-    confirmPassword: new FormControl('', [Validators.required,Validators.minLength(5),Validators.maxLength(12)]),
-  });
+    confirmPassword:  new FormControl('',Validators.required),
+  
+} 
+
+) 
+submitted = false;
+  
   
   onSubmit() { 
         console.log(this.registerForm.value);
+
+        this.submitted = true;
+        if (this.registerForm.invalid) {
+          return;
+      }
   }
 
   get userFirstName() 
@@ -40,6 +50,34 @@ export class RegisterComponent {
   get userPassword() {
     return this.registerForm.get('password');
   }
+
+  get userConfPassword() {
+    return this.registerForm.get('confirmPassword');
+  }
+
+  // MustMatch(password:any, confirmPassword:any) {
+  //     const passwordControl = FormGroup.controls[password];
+  //     const confirmPasswordControl = FormGroup.controls[confirmPassword];
+
+  //     if (confirmPasswordControl.error && !confirmPasswordControl.error['MustMatch']){
+  //       return
+  //     }
+  //     if (passwordControl.value !== confirmPasswordControl.value){
+  //       confirmPasswordControl.setError({MustMatch:true})
+  //     }else {
+  //       confirmPasswordControl.setError(null);
+  //     }
+  //  }
   
+   
+
+  saveList(isValid: boolean) {
+    if (isValid) {
+      console.log(this.registerForm)
+    }
+
+  }
+ 
+
 }
  
