fee39078a6392b40183913520ca110fb2884aef5
Sunil Salaria <hardeep.singh@lpinfotech.com>
2023-05-31 20:09:36 +0530

    update home page

--- e-book/src/app/pages/home/home.component.ts
+++ e-book/src/app/pages/home/home.component.ts
@@ -1,10 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { environment } from 'src/environments/environment.development';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent { 
+  table:any;
+  constructor(private http: HttpClient,) {}
+  ngOnInit() { 
+    this.http.get<any>(environment.userApi + 'user').subscribe((data =>{
+      console.log(data)
+      // this.table = data;
+      if(data === true) {
+        this.table.append('<tr><td></td></tr>');
+      }
+    }))
+    
+  }
+
 
 }
