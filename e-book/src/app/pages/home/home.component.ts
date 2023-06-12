e6d9280ab0b690e56dcd92cc4cff41cc5be03cb0
Sunil Salaria <hardeep.singh@lpinfotech.com>
2023-06-12 19:05:06 +0530

    update home page active card wishlist

--- e-book/src/app/pages/home/home.component.ts
+++ e-book/src/app/pages/home/home.component.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';  
+import { Component } from '@angular/core';
 import { ProductService } from 'src/app/services/product/product.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent { 
-  productImg:any; 
-  constructor(private productServices: ProductService){}
+export class HomeComponent  {
+  productImg: any;    
+  constructor(private productServices: ProductService) {}
   // this is get Product Image API
   ngOnInit() {
+    this.fetchCardsFromApi();
+  }
+  
+  fetchCardsFromApi() {
     this.productServices.getProducts().subscribe((data: any) => {
       this.productImg = data;
-    })
+    });
   }
-}
+
+  // active class
+  wishlistToggle(event: any) {
+    const activeClass = event.srcElement.classList.contains('CardWishList');
+    const classFound = document.querySelector('card');
+    const card = classFound?.classList.contains('CardWishList');
+    if (activeClass == true) {
+      if (card == true) {
+        classFound?.classList.remove('CardWishList');
+      }
+      event.srcElement.classList.remove('CardWishList');
+    } else {
+      if (card == true) {
+        classFound?.classList.remove('CardWishList');
+      }
+      event.srcElement.classList.add('CardWishList');
+    }
+    let activeLength = document.querySelectorAll('.CardWishList')
+    console.log((activeLength).length)
+  }
  
+}
